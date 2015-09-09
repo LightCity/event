53898dadd3617c5300c1482e3e361fde3ffd0809
Longli <longcpp@gmail.com>
2015-09-09 12:49:38 +0800

    tmp

--- app/src/main/java/event/longli/me/event/MainActivity.java
+++ app/src/main/java/event/longli/me/event/MainActivity.java
@@ -3,13 +3,9 @@ package event.longli.me.event;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewTreeObserver;
-import android.view.accessibility.AccessibilityEvent;
 
 
 public class MainActivity extends Activity {
